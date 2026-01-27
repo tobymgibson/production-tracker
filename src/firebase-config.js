69a890663ad3a5bcf3df50e5b9116f0ba3090c42
tobymgibson <tobygibson33@gmail.com>
2026-01-27 16:01:33 +0000

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,9 +9,6 @@ export const FIREBASE_CONFIG = {
   storageBucket: "production-tracker-ea9c5.firebasestorage.app",
   messagingSenderId: "949532992733",
   appId: "1:949532992733:web:84d2c48485d13b72ade734",
-  
-  // Optional: Realtime Database URL (if using Realtime Database instead of Firestore)
-  databaseURL: "https://docs.google.com/spreadsheets/d/1JI2Tp5epzEKE9bo1VvXDa8aE_gA_Vu2_jsiI57_VcNM/edit?gid=24401925#gid=24401925"
 };
 
 // Collection names
